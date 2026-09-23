ada964e54d0fe3797bf8aede78abb10459dc42d4
Halimi Halim <halimi3525@gmail.com>
2026-09-23 09:41:38 +0800

    Fit Selawat text on one line for mobile

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "uwa-bacaan-harian-v12-selawat-two-column";
+const CACHE_NAME = "uwa-bacaan-harian-v13-selawat-single-line";
 const INDEX_URL = "./index.html";
 const ROOT_URL = "./";
 const APP_SHELL = [
